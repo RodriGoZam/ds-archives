7124d705711b71e5cb35fbedd80fed6edefbfd14
Rodrigo <rodrigowzp@gmail.com>
2020-06-02 18:12:03 -0400

    Script Updated

--- shell1.sh
+++ shell1.sh
@@ -1,5 +1,4 @@
 #!/bin/sh 
-THEHOST = "alpine1"
 echo $THEHOST  # prints the name of the host to ping sleep 5        
 sleep 5        # wait some seconds ping $THEHOST  # ping it! 
 ping $THEHOST  #ping it!
