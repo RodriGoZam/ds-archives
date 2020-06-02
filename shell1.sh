538676e70de1791fb1c7f9c46c294313825e342a
Rodrigo <rodrigowzp@gmail.com>
2020-06-02 18:59:04 -0400

    Dockerfile Modified

--- shell1.sh
+++ shell1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 echo $THEHOST  # prints the name of the host to ping sleep 5        
 sleep 5        # wait some seconds ping $THEHOST  # ping it! 
-ping $THEHOST  #ping it! s
+ping $THEHOST  #ping it!
